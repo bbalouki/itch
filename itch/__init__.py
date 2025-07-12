1748d56d77bfae9761857220efc9de7d10fd29b6
bbalouki <bbalouki@outlook.com>
2025-07-12 10:33:06 +0000

    Update itchfeed

--- itch/__init__.py
+++ itch/__init__.py
@@ -6,5 +6,6 @@ __author__ = "Bertin Balouki SIMYELI"
 __copyright__ = "2025 Bertin Balouki SIMYELI"
 __email__ = "bertin@bbstrader.com"
 __license__ = "MIT"
-__version__ = "1.0.0"
+__version__ = "1.0.4"
+
 
